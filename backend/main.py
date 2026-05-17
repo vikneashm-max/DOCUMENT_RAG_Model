ef2b389a94afb6332e6c9847e99dfcd70d3fce9a
vikneashm-max <vikneashm@gmail.com>
2026-05-17 11:53:45 +0530

    Added some changes

--- backend/main.py
+++ backend/main.py
@@ -53,7 +53,11 @@ processor = DocumentProcessor()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://document-rag-model.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
